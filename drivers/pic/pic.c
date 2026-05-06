16d82af1ea0d8db8dffb54fbfdfb14b2b4fd9fab
tamier <perpectfieldvision@gmail.com>
2026-05-06 19:17:28 +0900

    Implement PIC IRQ management functions
    Added functions to disable IRQs, set and clear masks, and retrieve PIC registers.

--- drivers/pic/pic.c
+++ drivers/pic/pic.c
@@ -46,3 +46,73 @@ void pic_send_eoi(u8 irq) {
         outb(PIC2_CMD, 0x20);
     outb(PIC1_CMD, 0x20);
 }
+
+void pic_disable(void) {
+    // 모든 IRQ 마스크
+    outb(PIC1_DATA, 0xFF);
+    outb(PIC2_DATA, 0xFF);
+}
+
+static u16 __pic_get_reg(u8 ocw3) {
+    // master/slave 둘 다 같은 명령
+    outb(PIC1_CMD, ocw3);
+    outb(PIC2_CMD, ocw3);
+
+    u8 master = inb(PIC1_CMD);
+    u8 slave  = inb(PIC2_CMD);
+
+    return (slave << 8) | master;
+}
+
+u16 pic_get_irr(void) {
+    return __pic_get_reg(PIC_READ_IRR);
+}
+
+u16 pic_get_isr(void) {
+    return __pic_get_reg(PIC_READ_ISR);
+}
+
+u16 pic_get_mask(void) {
+    u8 master = inb(PIC1_DATA);
+    u8 slave  = inb(PIC2_DATA);
+
+    return (slave << 8) | master;
+}
+
+void pic_set_mask(u8 irq) {
+    u16 port;
+    u8 value;
+
+    if (irq < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irq -= 8;
+    }
+
+    value = inb(port) | (1 << irq);
+    outb(port, value);
+}
+
+void pic_clear_mask(u8 irq) {
+    u16 port;
+    u8 value;
+
+    if (irq < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irq -= 8;
+    }
+
+    value = inb(port) & ~(1 << irq);
+    outb(port, value);
+}
+
+u8 pic_irq_to_vector(u8 irq) {
+    if (irq < 8) {
+        return g_pic_offset1 + irq;
+    } else {
+        return g_pic_offset2 + (irq - 8);
+    }
+}
