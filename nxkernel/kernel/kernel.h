2d251ec56830daa112e3bafa7a0c12015952a386
plertube2bd <perpectfieldvision@gmail.com>
2026-05-16 19:32:15 +0900

    .

--- nxkernel/kernel/kernel.h
+++ nxkernel/kernel/kernel.h
@@ -1,11 +1,10 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#include "types.h"
-#include "boot_info.h"
+#include "include/types.h"
 
 // Kernel main function - called from assembly entry point (_start)
-interrupt void kernel_main(NTBLI* boot_info);
+void kernel_main(NTBLI* boot_info);
 
 // Get kernel information (restricted access)
 NTBLI* get_kernel_info(void);
